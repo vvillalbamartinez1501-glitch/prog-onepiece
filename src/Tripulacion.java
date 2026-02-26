3233ebb6ff5bf1cbe6386eba57b2883676779209
V Villalba Martínez <vvillalbamartinez1501@gmail.com>
2026-02-26 17:55:09 +0100

    feat(Tripulacion): implementa reclutarPirata con control de duplicados

---
+++ src/Tripulacion.java
@@ -0,0 +1,77 @@
+public class Tripulacion {
+    private String nombre;
+    private Barco barco;
+    private Pirata[] tripulacion;
+    private int capitanId;
+
+    private int MAX_TRIPULACION = 10;
+    private int numeroPiratas = 0;
+
+    public Tripulacion (String nombre, Barco barco, int capitanId) {
+        this.nombre = nombre;
+        this.barco = barco;
+        this.capitanId = capitanId;
+        this.tripulacion = new Pirata[MAX_TRIPULACION];
+    }
+
+    public boolean pirataEstaEnTripulacion(int idPirataBuscado){
+        boolean estaEnTripulacion = false;
+
+        for (int i = 0 ; !estaEnTripulacion && i < MAX_TRIPULACION ; i++){
+            if (tripulacion[i].getId() == idPirataBuscado){
+                estaEnTripulacion = true;
+            }
+        }
+        return estaEnTripulacion;
+    }
+
+    public int buscarIndicePirata(int idPirataBuscado){
+        int posicionPirataEnTripulacion = -1;
+
+        for (int i = 0 ; posicionPirataEnTripulacion == -1 && i < MAX_TRIPULACION ; i++){
+            if (tripulacion[i].getId() == idPirataBuscado){
+                posicionPirataEnTripulacion = i;
+            }
+        }
+        return posicionPirataEnTripulacion;
+    }
+
+    public boolean reclutarPirata(Pirata pirataAReclutar){
+        boolean pirataReclutado= true;
+        int posicionPirataEnTripulacion;
+
+        posicionPirataEnTripulacion = buscarIndicePirata(pirataAReclutar.getId());
+
+        if (posicionPirataEnTripulacion != -1){
+            pirataReclutado = false;
+        } else {
+            posicionPirataEnTripulacion = buscarHuecoLibreTripulacion();
+            tripulacion[posicionPirataEnTripulacion] = pirataAReclutar;
+        }
+        return pirataReclutado;
+    }
+
+    private int buscarHuecoLibreTripulacion(){
+        int posicionLibre = -1;
+        for (int i = 0 ;posicionLibre == -1 &&  i < MAX_TRIPULACION ; i++){
+            if (tripulacion[i] == null){
+                posicionLibre = i;
+            }
+        }
+        return posicionLibre;
+    }
+
+    public Pirata expulsarPirata(int idPirataAExpulsar){
+        Pirata pirataExpulsado = null;
+        int posicionPirataEnTripulacion;
+
+        if(pirataEstaEnTripulacion(idPirataAExpulsar)){
+            posicionPirataEnTripulacion = buscarIndicePirata(idPirataAExpulsar);
+
+        }
+
+
+        return pirataExpulsado;
+    }
+
+}
