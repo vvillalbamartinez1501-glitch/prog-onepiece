88633127bab0408b8f2ab74d319319d4600571ca
V Villalba Martínez <vvillalbamartinez1501@gmail.com>
2026-02-26 18:08:47 +0100

    feat(Tripulacion): implementa generarInforme con resumen completo de tripulacion

--- src/Tripulacion.java
+++ src/Tripulacion.java
@@ -47,6 +47,7 @@ public class Tripulacion {
         } else {
             posicionPirataEnTripulacion = buscarHuecoLibreTripulacion();
             tripulacion[posicionPirataEnTripulacion] = pirataAReclutar;
+            numeroPiratas++;
         }
         return pirataReclutado;
     }
@@ -70,6 +71,7 @@ public class Tripulacion {
         if(posicionPirataEnTripulacion != -1){
             pirataExpulsado = tripulacion[posicionPirataEnTripulacion];
             tripulacion[posicionPirataEnTripulacion] = null;
+            numeroPiratas--;
         }
 
         return pirataExpulsado;
@@ -88,6 +90,45 @@ public class Tripulacion {
         }
 
         return capitanActualizado;
+    }
+
+    public String generarInforme(){
+        StringBuilder informe = new StringBuilder("Informe completo de la tripulacion:");
+
+        informe.append("Nombre de la tripulacion: ");
+        informe.append(this.nombre);
+        informe.append("\n");
+
+        informe.append("Nombre del barco: ");
+        informe.append(this.barco.getNombre());
+        informe.append("\n");
+
+        informe.append("Número de miembros / Capacidad del barco: ");
+        informe.append(numeroPiratas);
+        informe.append("/");
+        informe.append(MAX_TRIPULACION);
+        informe.append("\n");
+
+        informe.append("Capitán del barco: ");
+        informe.append(tripulacion[capitanId].getNombre());
+        informe.append("\n");
+
+        informe.append("Listado de piratas: ");
+        informe.append("\n");
+
+        if (numeroPiratas == 0){
+            informe.append("No hay tripulación abordo de este barco");
+        } else {
+            for (int i = 0 ; i < MAX_TRIPULACION ; i++){
+                if (tripulacion[i] == null){
+                    informe.append(tripulacion[i].toString());
+                    informe.append("\n");
+
+                }
+            }
+        }
+
+        return informe.toString();
     }
 
 }
